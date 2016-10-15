4935d4a8fce285b85eedd9bc3d332aab1633fd16
Solomka <sol.yaremko@gmail.com>
2016-10-15 17:25:16 +0300

    final xml Spring config

--- src/main/java/ukma/eCommerce/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package ukma.eCommerce.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
