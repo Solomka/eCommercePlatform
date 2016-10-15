d939dbb3228a1742d863762bd191d37c958de6fe
Solomka <sol.yaremko@gmail.com>
2016-10-15 18:41:31 +0300

    corrected loading problem =)

--- src/main/java/ukma/eCommerce/core/paymentModule/domainLogic/ChargeManager.java
+++ src/main/java/ukma/eCommerce/core/paymentModule/domainLogic/ChargeManager.java
@@ -5,12 +5,15 @@ import java.util.Collection;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 
+import org.springframework.stereotype.Component;
+
 import ukma.eCommerce.core.paymentModule.model.domain.bo.Charge;
 import ukma.eCommerce.core.paymentModule.model.domain.vo.ChargeVO;
 import ukma.eCommerce.core.userModule.model.domain.bo.Seller;
 import ukma.eCommerce.util.IRetrieveCallback;
 import ukma.eCommerce.util.filter.ChargeFilter;
 
+@Component("chargeManager")
 public class ChargeManager implements IChargeManager {
 
 	@Override
