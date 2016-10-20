87219b69c26ef15f04b85268b2f4875c48f49667
Solomka <sol.yaremko@gmail.com>
2016-10-20 03:48:04 +0300

    Hibernate config added

---
+++ src/main/java/ukma/eCommerce/core/paymentModule/repository/po/OrderPO.java
@@ -0,0 +1,8 @@
+package ukma.eCommerce.core.paymentModule.repository.po;
+
+import javax.persistence.Entity;
+
+@Entity
+public class OrderPO {
+
+}
---
+++ src/main/java/ukma/eCommerce/core/userModule/repository/po/CustomerPO.java
@@ -0,0 +1,101 @@
+package ukma.eCommerce.core.userModule.repository.po;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class CustomerPO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4161497809953626589L;
+
+	@Id
+	@Column(name = "customer_id")
+	@GeneratedValue
+	private long id;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	@Column(name = "phone", nullable = false)
+	private String phone;
+
+	@Column(name = "login", nullable = false)
+	private String login;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
