f65e73e12722286380cc91deb431666b1dbd3731
Solomka <sol.yaremko@gmail.com>
2016-10-20 21:53:37 +0300

    deleted unused validation classes

--- src/main/java/ukma/eCommerce/core/paymentModule/model/dwo/OrderDTO.java
+++ src/main/java/ukma/eCommerce/core/paymentModule/model/dwo/OrderDTO.java
@@ -2,20 +2,14 @@ package ukma.eCommerce.core.paymentModule.model.dwo;
 
 import java.io.Serializable;
 
-import ukma.eCommerce.core.userModule.model.domain.vo.CustomerVO;
-import ukma.eCommerce.core.userModule.model.domain.vo.SellerVO;
-
 /**
  * Created by Максим on 10/8/2016.
  */
-public class OrderDTO implements Serializable{
+public class OrderDTO implements Serializable {
 	/*
-	private final CustomerVO customer;
-	private final SellerVO seller;
-	private final Shipment shipment;
-	private final Collection<OrderItemDTO> orderItems;
-	private final OrderStatus orderStatus;
-	private final DateTime creationDate;
-	private final DateTime fulfilmentDate;
-*/
+	 * private final CustomerVO customer; private final SellerVO seller; private
+	 * final Shipment shipment; private final Collection<OrderItemDTO>
+	 * orderItems; private final OrderStatus orderStatus; private final DateTime
+	 * creationDate; private final DateTime fulfilmentDate;
+	 */
 }
