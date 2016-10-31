320d57b89a20d903f63b20782dffcad59327816b
Solomka <sol.yaremko@gmail.com>
2016-10-31 22:11:14 +0200

    updated db structure

---
+++ ecommerce.sql
@@ -0,0 +1,348 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Окт 31 2016 г., 22:10
+-- Версия сервера: 5.1.41
+-- Версия PHP: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `ecommerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `account`
+--
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `stripeAccId` varchar(128) NOT NULL,
+  `seller_id` char(36) NOT NULL,
+  PRIMARY KEY (`stripeAccId`),
+  KEY `seller_id` (`seller_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `account`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `address`
+--
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` char(36) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `region` varchar(50) DEFAULT NULL,
+  `city` varchar(50) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `index` varchar(8) NOT NULL,
+  `phone` varchar(16) NOT NULL,
+  `first_name` varchar(35) NOT NULL,
+  `last_name` varchar(35) NOT NULL,
+  `customer_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `address`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` char(36) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `category`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `customer`
+--
+
+CREATE TABLE IF NOT EXISTS `customer` (
+  `id` char(36) NOT NULL,
+  `first_name` varchar(35) NOT NULL,
+  `last_name` varchar(35) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(16) NOT NULL,
+  `login` varchar(15) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `customer`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `invoice`
+--
+
+CREATE TABLE IF NOT EXISTS `invoice` (
+  `id` char(36) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `total_quantity` int(11) NOT NULL,
+  `total_sum` decimal(13,2) NOT NULL,
+  `currency` enum('UAH','EU','USD') NOT NULL DEFAULT 'USD',
+  `status` enum('PENDING','PAID','NOT_PAID') NOT NULL DEFAULT 'PENDING',
+  `order_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `invoice`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order`
+--
+
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` char(36) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `fulfilment_date` datetime DEFAULT NULL,
+  `status` enum('CREATED','PAID','CANCELED','RETURNED','FULFILLED_PARTIALLY','ISSUED_PARTIALLY','FULFILLED','ISSUED') NOT NULL DEFAULT 'CREATED',
+  `customer_id` char(36) NOT NULL,
+  `shipment_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `shipment_id` (`shipment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `order`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order_item`
+--
+
+CREATE TABLE IF NOT EXISTS `order_item` (
+  `order_id` char(36) NOT NULL,
+  `product_id` char(36) NOT NULL,
+  `total_quantity` int(11) NOT NULL,
+  `total_sum` decimal(13,2) NOT NULL,
+  PRIMARY KEY (`order_id`,`product_id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `order_item`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `payment`
+--
+
+CREATE TABLE IF NOT EXISTS `payment` (
+  `id` char(36) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `status` enum('PENDING','SUCCEEDED','FAILED,RETURNED') NOT NULL DEFAULT 'PENDING',
+  `invoice_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `invoice_id` (`invoice_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `payment`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` char(36) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` decimal(13,2) NOT NULL,
+  `currency` enum('UAH','EU','USD') NOT NULL DEFAULT 'USD',
+  `quantity` int(11) NOT NULL DEFAULT '0',
+  `description` varchar(512) NOT NULL,
+  `seller_id` char(36) NOT NULL,
+  `type_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `seller_id` (`seller_id`),
+  KEY `type_id` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `product`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `seller`
+--
+
+CREATE TABLE IF NOT EXISTS `seller` (
+  `id` char(36) NOT NULL,
+  `business_name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(16) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `region` varchar(50) DEFAULT NULL,
+  `city` varchar(50) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `index` varchar(8) NOT NULL,
+  `login` varchar(15) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `seller`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `shipment`
+--
+
+CREATE TABLE IF NOT EXISTS `shipment` (
+  `id` char(36) NOT NULL,
+  `delivery_service` varchar(50) DEFAULT NULL,
+  `total_sum` decimal(13,2) NOT NULL DEFAULT '0.00',
+  `delivery_date` datetime DEFAULT NULL,
+  `status` enum('IN_SENDER_WAREHOUSE','IN_DELIVERY','DELIVERED_PARTIALLY','DELIVERED') NOT NULL DEFAULT 'IN_SENDER_WAREHOUSE',
+  `address_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `address_id` (`address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `shipment`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `type`
+--
+
+CREATE TABLE IF NOT EXISTS `type` (
+  `id` char(36) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `category_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `type`
+--
+
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `invoice`
+--
+ALTER TABLE `invoice`
+  ADD CONSTRAINT `invoice_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_ibfk_4` FOREIGN KEY (`shipment_id`) REFERENCES `shipment` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `order_item`
+--
+ALTER TABLE `order_item`
+  ADD CONSTRAINT `order_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_item_ibfk_3` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `invoice` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_3` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_ibfk_4` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `shipment`
+--
+ALTER TABLE `shipment`
+  ADD CONSTRAINT `shipment_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `type`
+--
+ALTER TABLE `type`
+  ADD CONSTRAINT `type_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
